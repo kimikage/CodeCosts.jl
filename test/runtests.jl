b919824ce6970da6afe82e1291766952ca9b46b7
kimikage <kimikage.ceo@gmail.com>
2020-09-01 01:04:03 +0900

    Reduce misalignment within multi-line statements (#12)

--- test/runtests.jl
+++ test/runtests.jl
@@ -12,6 +12,8 @@ end
 
 @inline func_000(x::T) where T = (x + 1/3, x - oneunit(T), x / 3.0, x * 5.0)
 
+func_001(x) = :(if $x > 0; true; else false; end)
+
 buf = IOBuffer()
 
 v"1.6.0-DEV.0" <= VERSION < v"1.7.0-DEV.0" && @testset "Basic v1.6" begin
@@ -35,6 +37,16 @@ v"1.6.0-DEV.0" <= VERSION < v"1.7.0-DEV.0" && @testset "Basic v1.6" begin
         show(res)
         println()
         @test costs(res) == [1, 1, 20, 4, 0, 0]
+    end
+
+    @testset "func_001" begin
+        res = @code_costs func_001(1)
+        show(res)
+        println()
+
+        print(buf, res)
+        @test occursin(r"\n   0 └──      return %\d\n", String(take!(buf)))
+        @test costs(res) == [100, 100, 100, 100, 0]
     end
 end
 
@@ -60,6 +72,16 @@ v"1.5.0-DEV.0" <= VERSION < v"1.6.0-DEV.0" && @testset "Basic v1.5" begin
         println()
         @test costs(res) == [1, 1, 20, 4, 0, 0]
     end
+
+    @testset "func_001" begin
+        res = @code_costs func_001(1)
+        show(res)
+        println()
+
+        print(buf, res)
+        @test occursin(r"\n   0 └──      return %\d\n", String(take!(buf)))
+        @test costs(res) == [100, 100, 100, 100, 0]
+    end
 end
 
 v"1.4" <= VERSION < v"1.5.0-DEV.0" && @testset "Basic v1.4" begin
@@ -84,6 +106,16 @@ v"1.4" <= VERSION < v"1.5.0-DEV.0" && @testset "Basic v1.4" begin
         println()
         @test costs(res) == [1, 1, 20, 4, 0, 0]
     end
+
+    @testset "func_001" begin
+        res = @code_costs func_001(1)
+        show(res)
+        println()
+
+        print(buf, res)
+        @test occursin(r"\n   0 └──      return %\d\n", String(take!(buf)))
+        @test costs(res) == [100, 100, 100, 100, 0]
+    end
 end
 
 v"1.0" <= VERSION < v"1.1" && @testset "Basic v1.0" begin
@@ -106,5 +138,15 @@ v"1.0" <= VERSION < v"1.1" && @testset "Basic v1.0" begin
         show(res)
         println()
         @test costs(res) == [1, 1, 20, 4, 0, 0]
+    end
+
+    @testset "func_001" begin
+        res = @code_costs func_001(1)
+        show(res)
+        println()
+
+        print(buf, res)
+        @test occursin(r"\n   0    └──      return %\d\n", String(take!(buf)))
+        @test costs(res) == [100, 100, 100, 100, 0]
     end
 end
